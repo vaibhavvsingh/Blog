552141bb5893cd81d9538f0d3c711bd920ef6806
Vaibhav Singh <89960983+vaibhavvsingh@users.noreply.github.com>
2023-06-09 15:31:50 +0530

    updates

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -7,7 +7,7 @@ export default function Menu({ cat }) {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const res = await axios.get(`/posts/?cat=${cat}`);
+        const res = await axios.get(`${process.env.API_URL}/posts/?cat=${cat}`);
         setPosts(res.data);
       } catch (err) {
         console.log(err);
